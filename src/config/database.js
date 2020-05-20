fe8549dce3cfabd56124878eb2acd44d44a0aa57
Jorge Igor Alves Rodrigues Silva <jorgeigor.1995@gmail.com>
2020-05-20 00:37:51 -0300

    criando repositorio

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'root',
+  password: 'root',
+  database: 'nodeauth',
+  dialect: 'mysql',
+  operatorsAlises: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  },
+}
